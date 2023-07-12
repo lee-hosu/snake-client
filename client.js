e17f0b9913109dbd9453cb73a66e9c84953aa78d
Hosu <sogeumhosu@gmail.com>
2023-07-12 09:30:28 -0700

    Refactor: Create client.js to separate connect function

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("You are connected! 🐍");
+  });
+  conn.on("data", (data) => {
+    console.log("Received data:", data);
+  });
+  return conn;
+};
+
+module.exports = {
+  connect,
+};
--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("You are connected! 🐍");
-  });
-  conn.on("data", (data) => {
-    console.log("Received data:", data);
-  });
-  return conn;
-};
+const client = require("./client");
 
 console.log("Connecting ...");
-connect();
+client.connect();
