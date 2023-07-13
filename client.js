6870a84e5fc3eb15a09b73a2c623661681a8e3d2
Hosu <sogeumhosu@gmail.com>
2023-07-12 19:15:21 -0700

    Practice adding snake movement commands

--- client.js
+++ client.js
@@ -10,8 +10,23 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("You are connected! 🐍");
+
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 2000);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // }, 3000);
   });
+
   conn.write("Name: HSL");
+
   conn.on("data", (data) => {
     console.log("Received data:", data);
   });
