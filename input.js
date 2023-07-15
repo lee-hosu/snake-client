99ce5dd2814d6a0beab18fe8fe95a81f5a4c8588
Hosu <sogeumhosu@gmail.com>
2023-07-14 19:27:27 -0700

    Refactor: create input.js file

---
+++ input.js
@@ -0,0 +1,24 @@
+// setup interface to handle user input from stdin
+const handleUserInput = function (key) {
+  process.stdout.write("key down");
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
+
+module.exports = {
+  setupInput,
+};
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const client = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-client.connect();
-
-// setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  process.stdout.write("key down");
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+connect();
 
 setupInput();
